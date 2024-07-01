caa0229bfee17de813b6081405c4eddc7568fb34
theveetz <nikkipersonal2@gmail.com>
2024-07-01 17:22:52 +0930

    Create Analytics.js

---
+++ src/Pages/Analytics.js
@@ -0,0 +1,13 @@
+Filename: src/pages/Analytics.js
+
+import React from 'react';
+
+const Analytics = () => {
+  return (
+    <div>
+      <h2>Analytics</h2>
+    </div>
+  );
+};
+
+export default Analytics;
