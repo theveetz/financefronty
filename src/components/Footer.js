90149ab1906c70a50259b87c28a440aa656e9e97
theveetz <nikkipersonal2@gmail.com>
2024-07-01 17:21:10 +0930

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+Filename: src/components/Footer.js
+
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>&copy; 2024 Finance Dashboard</p>
+    </footer>
+  );
+};
+
+export default Footer;
