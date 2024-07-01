13a7c3e4886ca1c83cc985875916379ee2074b72
theveetz <nikkipersonal2@gmail.com>
2024-07-01 17:24:11 +0930

    Create Settings.js

---
+++ src/Pages/Settings.js
@@ -0,0 +1,13 @@
+Filename: src/pages/Settings.js
+
+import React from 'react';
+
+const Settings = () => {
+  return (
+    <div>
+      <h2>Settings</h2>
+    </div>
+  );
+};
+
+export default Settings;
