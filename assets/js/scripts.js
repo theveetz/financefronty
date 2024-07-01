c9390e22e0cf2ba9a4006bbd76ea1c54bc862d92
theveetz <nikkipersonal2@gmail.com>
2024-07-01 16:56:09 +0930

    Create scripts.js

---
+++ assets/js/scripts.js
@@ -0,0 +1,7 @@
+document.querySelector('.btn-primary').addEventListener('click', () => {
+    alert('Add Transaction button clicked!');
+});
+
+document.querySelector('.btn-secondary').addEventListener('click', () => {
+    alert('Generate Report button clicked!');
+});
