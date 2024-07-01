8636526284040c59e4e8958859ac48cd26510eb8
theveetz <nikkipersonal2@gmail.com>
2024-07-01 17:51:22 +0930

    Update SignUp.js

--- src/Pages/SignUp.js
+++ src/Pages/SignUp.js
@@ -1,11 +1,9 @@
-Filename: src/pages/SignUp.js
-
-import React from 'react';
+\import React from 'react';
 
 const SignUp = () => {
   return (
     <div>
-      <h2>Sign Up</h2>
+      <h1>Sign Up</h1>
     </div>
   );
 };
