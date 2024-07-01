6faa8739a11d6098493b6ad3e463836752314545
theveetz <nikkipersonal2@gmail.com>
2024-07-01 17:08:00 +0930

    Delete assets directory

--- assets/js/scripts.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('.btn-primary').addEventListener('click', () => {
-    alert('Add Transaction button clicked!');
-});
-
-document.querySelector('.btn-secondary').addEventListener('click', () => {
-    alert('Generate Report button clicked!');
-});
