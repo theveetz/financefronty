1ed98878772c6349019b036035ad615ca588996b
theveetz <nikkipersonal2@gmail.com>
2024-07-01 17:21:43 +0930

    Create Navbar.js

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+Filename: src/components/Navbar.js
+
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1>Finance Dashboard</h1>
+    </nav>
+  );
+};
+
+export default Navbar;
