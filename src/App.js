8212384d932d084bebdb58c9210178f6a9a89561
theveetz <nikkipersonal2@gmail.com>
2024-07-01 17:10:38 +0930

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/main.css';
-import App from './App';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Login from './components/Auth/Login';
+import Signup from './components/Auth/Signup';
+import Overview from './components/Dashboard/Overview';
+import Transactions from './components/Dashboard/Transactions';
+import Analytics from './components/Dashboard/Analytics';
+import Profile from './components/Settings/Profile';
+import AccountSettings from './components/Settings/AccountSettings';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={Signup} />
+        <Route path="/dashboard/overview" component={Overview} />
+        <Route path="/dashboard/transactions" component={Transactions} />
+        <Route path="/dashboard/analytics" component={Analytics} />
+        <Route path="/settings/profile" component={Profile} />
+        <Route path="/settings/account" component={AccountSettings} />
+        <Route path="/" exact component={Overview} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
